3582b042cf0874bdfac2ff0a07ef2938ece61adf
isdood <calebjdt@proton.me>
2025-06-14 10:19:43 -0400

    Beginning a simple 'pong' game for learning purposes 🌌

--- experiments/pong/pong.asm
+++ experiments/pong/pong.asm
@@ -14,6 +14,7 @@ section .data
     CLEAR_SCREEN    db 27, '[2J', 27, '[H', 0
     HIDE_CURSOR     db 27, '[?25l', 0
     SHOW_CURSOR     db 27, '[?25h', 0
+    CURSOR_POS      db 27, '[', 0, ';', 0, 'H', 0  ; ANSI escape sequence for cursor positioning
 
 section .bss
     ; Game state
@@ -23,9 +24,98 @@ section .bss
     ball_y          resb 1    ; Y position of ball
     ball_dx         resb 1    ; Ball X direction (-1 or 1)
     ball_dy         resb 1    ; Ball Y direction (-1 or 1)
+    cursor_pos      resb 8    ; Buffer for cursor position string
 
 section .text
     global _start
+
+; Function to set cursor position
+; Input: r8b = row, r9b = column
+set_cursor_pos:
+    push rax
+    push rdi
+    push rsi
+    push rdx
+
+    ; Convert row and column to ASCII
+    mov rax, r8
+    add al, '0'
+    mov [CURSOR_POS + 2], al
+    mov rax, r9
+    add al, '0'
+    mov [CURSOR_POS + 4], al
+
+    ; Write cursor position sequence
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, CURSOR_POS
+    mov rdx, 7
+    syscall
+
+    pop rdx
+    pop rsi
+    pop rdi
+    pop rax
+    ret
+
+; Function to draw a paddle
+; Input: r8b = y position, r9b = x position
+draw_paddle:
+    push rax
+    push rdi
+    push rsi
+    push rdx
+    push rcx
+
+    mov cl, PADDLE_HEIGHT    ; Counter for paddle height
+.draw_paddle_loop:
+    ; Set cursor position
+    push rcx
+    call set_cursor_pos
+    pop rcx
+
+    ; Draw paddle character
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, PADDLE_CHAR
+    mov rdx, 1
+    syscall
+
+    ; Move to next line
+    inc r8b
+    dec cl
+    jnz .draw_paddle_loop
+
+    pop rcx
+    pop rdx
+    pop rsi
+    pop rdi
+    pop rax
+    ret
+
+; Function to draw the ball
+; Input: r8b = y position, r9b = x position
+draw_ball:
+    push rax
+    push rdi
+    push rsi
+    push rdx
+
+    ; Set cursor position
+    call set_cursor_pos
+
+    ; Draw ball character
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, BALL_CHAR
+    mov rdx, 1
+    syscall
+
+    pop rdx
+    pop rsi
+    pop rdi
+    pop rax
+    ret
 
 _start:
     ; Initialize game state
@@ -51,9 +141,20 @@ game_loop:
     mov rdx, 7
     syscall
 
-    ; TODO: Handle input
-    ; TODO: Update game state
-    ; TODO: Draw game elements
+    ; Draw left paddle
+    mov r8b, [left_paddle_y]
+    mov r9b, 2          ; X position for left paddle
+    call draw_paddle
+
+    ; Draw right paddle
+    mov r8b, [right_paddle_y]
+    mov r9b, 77         ; X position for right paddle
+    call draw_paddle
+
+    ; Draw ball
+    mov r8b, [ball_y]
+    mov r9b, [ball_x]
+    call draw_ball
 
     ; Small delay
     mov rax, 35
